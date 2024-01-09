b3ec05cd31e315c7b143dc9abf17ddba0470ad7c
DarrenChochian <dikran.d.c@outlook.com>
2024-01-09 02:46:29 -0800

    comments

--- TypingTest.py
+++ TypingTest.py
@@ -96,5 +96,5 @@ class TypingTestApp:
             self.personal_best_wpm = current_wpm
             self.personal_best_label.config(text="NEW PERSONAL BEST ★")
 
-#adding more soon
+#adding more soonn
 TypingTestApp()
